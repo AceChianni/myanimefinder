153ca34b7babb9ab66be7f893f4e724b02aacf6e
Your NameAce <chantricelacabe@gmail.com>
2024-11-27 22:30:43 -0600

    email template added

--- src/components/EmailTemplate.jsx
+++ src/components/EmailTemplate.jsx
@@ -1,15 +1,9 @@
 // components/EmailTemplate.jsx
 import React from "react";
-
+// components/EmailTemplate.js
 export const EmailTemplate = ({ name, email, phone, message }) => (
-  <div
-    style={{
-      fontFamily: "Arial, sans-serif",
-      color: "#333",
-      lineHeight: "1.6",
-    }}
-  >
-    <h1>New Contact Form Submission</h1>
+  <div>
+    <h1>Contact Form Submission</h1>
     <p>
       <strong>Name:</strong> {name}
     </p>
@@ -20,8 +14,7 @@ export const EmailTemplate = ({ name, email, phone, message }) => (
       <strong>Phone:</strong> {phone}
     </p>
     <p>
-      <strong>Message:</strong>
+      <strong>Message:</strong> {message}
     </p>
-    <p>{message}</p>
   </div>
 );
