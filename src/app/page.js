aa3ff20bdcb9f9876143d551bc58dd0ab6d02824
Your NameAce <chantricelacabe@gmail.com>
2024-11-28 00:50:44 -0600

    contact form positions

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,13 @@
-// // src/app/page.js
-// import React from "react";
-// import LeftSidebar from "../components/Sidebar";
-// import Slideshow from "../components/Slideshow";
-// import RightSidebar from "../components/PollSidebar";
-// import "../styles/globals.css"; // Global styles
-
-// export default function HomePage() {
-//   return (
-//     <div>
-//       {/* Main page layout */}
-//       <main className="flex flex-col lg:flex-row gap-4 min-h-screen p-4">
-//         {/* Left Sidebar */}
-//         <div className="lg:w-1/4 p-4 bg-sage-green rounded-lg shadow-md"></div>
-
-//         {/* Slideshow in the center */}
-//         <div className="flex-1 flex justify-center items-center">
-//           <div className="w-full max-w-6xl"></div>
-//         </div>
-//       </main>
-//     </div>
-//   );
-// }
 // src/app/page.js
 import React from "react";
 import "../styles/globals.css";
 
 export default function HomePage() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-comic text-pink-500 text-center pt-12">
+        Annii's Anime Recommendations
+      </h1>
+    </div>
+  );
 }
